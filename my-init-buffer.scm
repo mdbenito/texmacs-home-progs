e383d1695d741cdabc05633ae6ecbe26f8eb9792
Ana Cañizares <ana2588@gmail.com>
2017-05-24 00:02:22 +0200

    Add my-init-buffer and my-init-texmacs to private branch

--- my-init-buffer.scm
+++ my-init-buffer.scm
@@ -1,14 +1,5 @@
-; hey
-
-;(define MasterArbeitDir "/Users/miguel/Documents/Universidad/Two sided contact")
-
-;(if (== (dirname (url->string (current-buffer-url))) MasterArbeitDir)
-;    (kbd-map
-;      (:mode in-math?)
-;      ("macos i" (begin (make-script #t #f) (insert "<iota>") (go-right)))))
-
-;(if (url-scratch? (current-buffer))
-;    (begin
-;      (set-style-list (append (get-style-list) (list "Snippet")))
-;      (buffer-pretend-saved (current-buffer))))
-    
+(with b (current-buffer)
+  (if (url-scratch? b)
+      (begin
+        (set-style-list (append (get-style-list) (list "Pilstyle")))
+        (buffer-pretend-saved b))))
