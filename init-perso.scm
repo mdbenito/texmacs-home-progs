751855af5be49975193036ce1ba0892637117d56
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2021-07-11 15:33:24 +0200

    Use the stuff

--- init-perso.scm
+++ init-perso.scm
@@ -1,9 +1,9 @@
 (load "init-kbd.scm")
-(load "scheme-tools.scm")
+(load "prog-tools.scm")
+(load "init-focusbar.scm")
 
 ;(load "init-slo.scm")
 ;(load "init-tools.scm")
 
 ; This is no longer necessary:
 ;(load "init-autocomplete.scm")
-(load "init-focusbar.scm")
